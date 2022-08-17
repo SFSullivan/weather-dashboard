e9a067b3b8e5a426425774b743b0acc23665f890
SSullivan <spencersullivan1990@gmail.com>
2022-08-16 20:16:16 -0500

    restarting the weather dashboard

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 var citySearch = document.querySelector("#citySubmit")
 var apiKey = "0342aa5e399ecd0cd610e5858ede6f30"
-function cityFetch(searchedCity) {
+var cityObj = coordinatesFetch(searchedCity)
+
+
+function coordinatesFetch(searchedCity) {
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchedCity + '&appid=' + apiKey)
-        .then(response => response.json())
-}
-citySearch.addEventListener("click", function() {
-    var searchedCity = document.querySelector("#searchedCity").value
-    cityFetch(searchedCity)
-    // .then(data => console.log(data)).finally(()=>console.log("test"));
-    console.log(searchedCity)
-})
+    .then(response => response.json())
+    .then(data => console.log(JSON.parse(JSON.stringify(data))))
+};

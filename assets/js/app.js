dc129c2645eb00fb515f588f8ac3b99d3a05e347
SSullivan <spencersullivan1990@gmail.com>
2022-08-18 21:26:34 -0500

    moment works for day 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,15 @@ $(document).ready(function() {
 
     var searchCity;
     var searchBttn = document.querySelector('#searchBttn')
+    var day1 = moment().format('MMMM Do');
     
+    //dates for cards
+
+    $('#first_day').text(day1);
+
+    
+
+
+
+
 })
